47e08163ae04c1232b0ca786e18a731877c82024
abeni-al7 <abenezeralebachew3@gmail.com>
2025-08-27 10:03:56 +0300

    Scaffold the project structure

---
+++ backend/delivery/api/routers/images.py
@@ -0,0 +1,4 @@
+from fastapi import APIRouter
+from delivery.api.controllers import images_controller
+
+router = APIRouter()
---
+++ backend/delivery/api/routers/schedule.py
@@ -0,0 +1,4 @@
+from fastapi import APIRouter
+from delivery.api.controllers import schedule_controller
+
+router = APIRouter()
