3946d76cedb8cbc697d352fdbf0b33d36818926f
ruthyeshitila12@gmail.com <167624441+ruye19@users.noreply.github.com>
2025-08-28 17:32:01 +0300

    auth started

--- mobile/socialspark_app/lib/features/authentication/data/repositories/auth_repository_impl.dart
+++ mobile/socialspark_app/lib/features/authentication/data/repositories/auth_repository_impl.dart
@@ -1,6 +1,6 @@
 import '../../domain/entities/user.dart';
 import '../../domain/repositories/auth_repository.dart';
-import '../datasources/auth_remote_datasource.dart';
+import '../datasources/remote/auth_remote_datasource.dart';
 import '../models/user_model.dart';
 
 class AuthRepositoryImpl implements AuthRepository {
